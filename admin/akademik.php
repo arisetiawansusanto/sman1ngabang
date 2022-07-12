78afffa8baf4229a7aca62a5c164d141d82bb5cc
arisetiawansusanto <109162968+arisetiawansusanto@users.noreply.github.com>
2022-07-12 23:28:59 +0700

    Update akademik.php

--- admin/akademik.php
+++ admin/akademik.php
@@ -32,7 +32,7 @@ include('../config/cAkademik.php');
     <div class="dashboard-main-wrapper">
         <div class="dashboard-header">
             <nav class="navbar navbar-expand-lg bg-white fixed-top">
-                <a class="navbar-brand" href="index.php">SMA 1 NGABANGasdasd</a>
+                <a class="navbar-brand" href="index.php">SMA 1 NGABANGasdasdASDA</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
